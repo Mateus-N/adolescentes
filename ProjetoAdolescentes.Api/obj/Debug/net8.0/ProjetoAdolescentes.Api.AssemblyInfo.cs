e3c67269e6f978152416ebd0f7b3796d31fe23b0
mateus nunes <mateusnunes620@gmail.com>
2025-04-22 22:41:15 -0300

    git ignore

--- ProjetoAdolescentes.Api/obj/Debug/net8.0/ProjetoAdolescentes.Api.AssemblyInfo.cs
+++ ProjetoAdolescentes.Api/obj/Debug/net8.0/ProjetoAdolescentes.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAdolescentes.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543881460fc4f82457f4c1c2cb734edc0ddfd42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAdolescentes.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAdolescentes.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
